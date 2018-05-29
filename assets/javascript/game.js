662c96a554e4fd46b3a507c1a634418b04d3053d
Cassindi <tara.devin.b@gmail.com>
2018-05-29 12:47:13 -0500

    making progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,10 +1,64 @@
-var warcraftWords = ["Jaina", "Thrall", "Sylvanas", "Khadgar", "Outland", "Draenor", "Pandaria", "Kalimdor", "Eastern Kingdoms", "Northrend", "Broken Isles", "Azeroth", "Orgrimmar", "Stormwind", "Dalaran", "Ironforge", "Thunderbluff", "Argus"];
+var warcraftWords = ["jaina", 
+                    "thrall", 
+                    "sylvanas", 
+                    "khadgar", 
+                    "outland", 
+                    "draenor", 
+                    "pandaria", 
+                    "kalimdor", 
+                    "eastern kingdoms", 
+                    "northrend", 
+                    "broken isles", 
+                    "azeroth", 
+                    "orgrimmar", 
+                    "stormwind", 
+                    "dalaran",
+                    "ironforge", 
+                    "thunderbluff", 
+                    "argus"]
+
+
+
+var randomWords = [];
+
+var usedLetters = [];
+
+var wrongCount = 0;
+
+document.onkeyup = function(event) {
+    var userGuess = event.key.toLowerCase();
+}
+
+var wordDiv = document.getElementById("random-word");
 
 var computerWord = warcraftWords[Math.floor(Math.random()*warcraftWords.length)];
 
 //Use onkeyup to detect user guess
 //Use a for loop and charAt to determine whether user guess matches letter in computer guess, cycling through length of word
-document.onkeyup = function(event) {
-    var userInput = event.key.toLowerCase();
+
+
+for (var i = 0; i < computerWord.length; i++){
+
+    if (computerWord[i] === ' ') {
+    randomWords.push(' ');
+    }
+
+    else {
+        randomWords.push('_ ');
+    }
+
+    wordDiv.textContent = randomWords.join("");
 }
 
+for (var j=0; j < computerWord.length; i++) {
+
+    if (computerWord[j] === userGuess){
+        randomWords[i] = computerWord[j];
+        wordDiv.textContent = randomWords.join("");
+    }
+    
+    else {
+        wrongCount++;
+        document.getElementById("letters").textContent = userGuess;
+    }
+}

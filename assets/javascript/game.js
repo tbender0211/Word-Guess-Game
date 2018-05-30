e153a746ba0a538525825d2dd78aa465cc81ee23
Cassindi <tara.devin.b@gmail.com>
2018-05-29 23:19:49 -0500

    struggling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -25,11 +25,10 @@ var usedLetters = [];
 
 var wrongCount = 0;
 
-var userGuess;
-
 document.onkeyup = function (event) {
-    var userGuess = event.key.toLowerCase();
-}
+    var userGuess = userInput(event.key.toLowerCase());
+    }
+
 
 var wordDiv = document.getElementById("random-word");
 
@@ -38,7 +37,7 @@ var computerWord = warcraftWords[Math.floor(Math.random()*warcraftWords.length)]
 //Use onkeyup to detect user guess
 //Use a for loop and charAt to determine whether user guess matches letter in computer guess, cycling through length of word
 
-for (var i = 0; i < computerWord.length; i++){
+for (i = 0; i < computerWord.length; i++){
 
     if (computerWord[i] === ' ') {
     randomWords.push(' ');
@@ -52,17 +51,26 @@ for (var i = 0; i < computerWord.length; i++){
 }
 
 document.getElementById("wrong-count").textContent = "Incorrect Guesses: " + wrongCount;
-document.getElementById("letters").textContent = "Letters Used:" + usedLetters.join("");
+document.getElementById("letters").textContent = "Letters Used: " + usedLetters.join("");
 
-function userGuess(letter){
-}
-//In this if function, need to figure out what to put where "onkeyup" is
-if (computerWord.indexOf(userGuess >= 0)) {
-    for (var i=0; i < computerWord.length; i++) {
 
-        if (computerWord[i] === onkeyup() ) {
-          randomWords[i] === computerWord[i];
+//Need to figure out how to write userGuess to randomWords array
+document.onkeyup = function(event) {
+
+        var userGuess = event.key.toLowerCase();
+        console.log(userGuess);
+
+        for (i=0; i < computerWord.length; i++); {
+
+            if (computerWord[i] == userGuess) {
+                userGuess == randomWords[i];
+                randomWords.push(computerWord[i]);
+                wordDiv.textContent = randomWords.join("");
+            }
+    
+            else {
+                wrongCount++;
+        }
     }
-}
-    wordDiv.textContent = randomWords.join("");
-}
+    }
+
